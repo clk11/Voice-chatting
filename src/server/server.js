07f7bdefbbd9c6f539251ed1d7769dda48263086
root <root@DESKTOP-2TAN30E>
2023-02-22 20:33:34 +0200

    Just built a box for the messages

--- src/server/server.js
+++ src/server/server.js
@@ -7,8 +7,27 @@ const config = require('config');
 // const db = require('./db');
 const auth = require('./middlewares/auth');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
 app.use(express.json({ extended: false }));
 app.use(cors());
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: null
+    }
+});
+
+io.on("connection", (socket) => {
+    socket.on("join_room", obj => {
+        socket.join(obj.room);
+    })
+    socket.on("send_message", obj => {
+        console.log(obj);
+        socket.broadcast.to(obj.room).emit("received_message", obj);
+    });    
+})
+
 app.post('/login', async (req, res) => {
     const ipAddress = req.socket.remoteAddress;
     const { username, room } = req.body;
@@ -32,24 +51,5 @@ app.get('/',auth,async(req,res)=>{
 		res.status(401).send({ msg: 'Permission denied !' });
 	}
 });
-// const io = new Server(server, {
-//     cors: {
-//         origin: "http://localhost:3000",
-//         methods: null
-//     }
-// });
 
-// io.on("connection", (socket) => {
-//     socket.on("join_room", room => {
-//         socket.join(room);
-//     })
-//     socket.on("send_message", obj => {
-//         socket.broadcast.to(obj.room).emit("received_message", obj);
-//         socket.emit("received_message",obj);
-//     });    
-// })
-//Enabling passing data to the body of the req
-
-
-// app.use('/login', require('./routes/login'));
 server.listen(3001, () => console.log(`Server started on port 3001`));
